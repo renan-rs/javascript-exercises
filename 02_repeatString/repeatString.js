267adf5b85dd5a368bcb3880c18e094675a68961
Renan Siqueira <renan.siqueira8a@gmail.com>
2024-11-24 03:49:37 -0300

    Exercise 02 solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(userString, repeatNumber) {
+    if(repeatNumber < 0){
+        return "ERROR";
+    }
+    let result = "";
+    for (let i = 0; i < repeatNumber; i++) {
+        result += userString;
+    }
+    return result;
 };
 
 // Do not edit below this line

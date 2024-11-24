10dfb97fe07bce81c962d85476fd199bac2dd355
Renan Siqueira <renan.siqueira8a@gmail.com>
2024-11-24 16:19:27 -0300

    Exercise 05 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(numStart, numEnd) {
+    if(typeof numStart != 'number' || typeof numEnd != 'number'){
+        return 'ERROR';
+    }
+    if(numStart < 0 || numEnd < 0){
+        return 'ERROR';
+    }
+    if(numStart % 1 != 0 || numEnd % 1 != 0){
+        return 'ERROR';
+    }
 
+    /* Sum = (numberOfTerms*(firstTerm+lastTerm)) / 2 */
+    let nTerms = numStart > numEnd ? numStart : numEnd;
+    let result = nTerms * (numStart+numEnd) / 2
+    return result;
 };
 
 // Do not edit below this line

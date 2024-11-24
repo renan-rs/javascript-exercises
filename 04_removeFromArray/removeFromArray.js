0199ed838b71efe7fdc45f48f802367f6055d32b
Renan Siqueira <renan.siqueira8a@gmail.com>
2024-11-24 05:13:59 -0300

    Exercise 04 solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(userArray, ...removeValues) {
+    let result = Array.from(userArray);
+    removeValues.forEach((arg) => {
+        result = result.filter((x) => x !== arg);  
+    })
+    return result;
 };
 
 // Do not edit below this line

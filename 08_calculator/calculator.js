eb7f99f944a46cf0c806b2c0539f719a5f0dd94d
Renan Siqueira <renan.siqueira8a@gmail.com>
2024-11-28 13:08:35 -0300

    Exercise 08 solution
    Factorial function done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -24,8 +24,11 @@ const power = function(num1, num2) {
   return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num <= 0){
+    return 1;
+  }
+  return num * factorial(num-1);
 };
 
 // Do not edit below this line

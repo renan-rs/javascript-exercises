05f72451f6d31c4b66b4c7a1909d95fcedd12f2c
Renan Siqueira <renan.siqueira8a@gmail.com>
2024-11-28 14:51:31 -0300

    Exercise 12 solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+function getAge(birth, death){
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
+const findTheOldest = function(arr) {
+    return arr.reduce((oldestPerson, currentPerson) => {
+        let oldestAge = getAge(
+            oldestPerson.yearOfBirth, 
+            oldestPerson.yearOfDeath);
+        let currentAge = getAge(
+            currentPerson.yearOfBirth, 
+            currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldestPerson;
+    });
 };
 
 // Do not edit below this line

c4dadb7284b97790938170b9dd34e89766325436
Renan Siqueira <renan.siqueira8a@gmail.com>
2024-11-28 14:04:59 -0300

    Exercise 09 solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strClean = str.trim().replace(/[\s+.,!?]/g, "").toLowerCase();
+    let strReversed = Array.from(strClean).toReversed().join("");
+    return strClean === strReversed;
 };
 
 // Do not edit below this line
